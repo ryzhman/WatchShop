1ca9b7bda8b5a6c74226bffe893d98804c93f4c2
Oleksandr Ryzhkov <ryzhman@gmail.com>
2017-11-04 09:39:12 +0200

    Adding AOP dependecies, configuration and aspect itself

---
+++ src/main/java/com/watchShop/aspects/ExceptionTransformationAspect.java
@@ -0,0 +1,10 @@
+package com.watchShop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Created by Oleksandr Ryzhkov on 04.11.2017.
+ */
+@Aspect
+public class ExceptionTransformationAspect {
+}
---
+++ src/main/java/com/watchShop/config/AopConfig.java
@@ -0,0 +1,12 @@
+package com.watchShop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by Oleksandr Ryzhkov on 04.11.2017.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+}
