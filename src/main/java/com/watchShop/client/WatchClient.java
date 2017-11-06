3aa29675ac17c59701a8ffb27d8cd403dd106a75
Oleksandr Ryzhkov <ryzhman@gmail.com>
2017-11-06 22:28:19 +0200

    Frame for client

---
+++ src/main/java/com/watchShop/client/WatchClient.java
@@ -0,0 +1,41 @@
+package com.watchShop.client;
+
+import com.watchShop.exception.GenericEngineException;
+import com.watchShop.service.WatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.Properties;
+
+/**
+ * Created by Oleksandr Ryzhkov on 06.11.2017.
+ */
+@Component
+public class WatchClient {
+    @Value("${systemProperty.dataServer}")
+    private String URL_SERVER_TO_GET_DATA_FROM;
+
+    private WatchService watchService;
+
+    @Autowired
+    public WatchClient(WatchService watchService) {
+        this.watchService = watchService;
+    }
+
+    public boolean fetchDataFromThirdPartyServer() throws GenericEngineException {
+        try {
+            //todo fetch data
+
+            //todo parse data
+            //todo store data
+
+
+            return true;
+        } catch (Exception e) {
+            throw new GenericEngineException("Could not fetch and store data from the third party server");
+        }
+    }
+}
--- src/main/java/com/watchShop/controller/WatchListController.java
+++ src/main/java/com/watchShop/controller/WatchListController.java
@@ -2,7 +2,9 @@ package com.watchShop.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.watchShop.client.WatchClient;
 import com.watchShop.entity.Status;
+import com.watchShop.exception.GenericEngineException;
 import com.watchShop.service.WatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,23 @@ import java.util.Optional;
 @RequestMapping("/v1/watches")
 public class WatchListController {
     private ObjectMapper mapper = new ObjectMapper();
-    @Autowired
     private WatchService watchService;
+    private WatchClient watchClient;
+
+    @Autowired
+    public WatchListController(WatchService watchService, WatchClient watchClient) {
+        this.watchService = watchService;
+        this.watchClient = watchClient;
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public JsonNode getAllWatches(@RequestParam("status") Optional<String> status) {
         Status status1 = Status.valueOf(status.orElse("AVAILABLE"));
         return mapper.valueToTree(watchService.getAllWatchesByStatus(status1));
+    }
+
+    @RequestMapping(value = "/thirdPartyWatches", method = RequestMethod.POST)
+    public boolean loadWatchesFromThirdPartyServer() throws GenericEngineException {
+        return watchClient.fetchDataFromThirdPartyServer();
     }
 }
